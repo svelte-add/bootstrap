707231f11008131a9ded2743b39f3e22be2ca986
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2022-01-10 14:43:15 -0500

    🧹 chore: refactor `__detect.js` and `__metadata.js` into `__info.js`

--- __info.js
+++ __info.js
@@ -1,5 +1,18 @@
 import { extension } from "../scss/stuff.js";
 
+export const name = "Bootstrap";
+
+/** @typedef {{ sveltestrap: boolean }} Options */
+
+/** @type {import("../..").AdderOptions<Options>} */
+export const options = {
+	sveltestrap: {
+		context: "It provides Svelte components with client-side interactivity in Bootstrap's style. https://sveltestrap.js.org/",
+		default: false,
+		question: "Do you want to install Sveltestrap?",
+	},
+};
+
 /** @type {import("../..").Heuristic[]} */
 export const heuristics = [
 	{
