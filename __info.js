94d1f3f567903f516ef3b2dfd85006bdb4847148
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2022-02-02 22:58:15 -0500

    ⚡️ feat+docs: generate READMEs

--- __info.js
+++ __info.js
@@ -2,6 +2,10 @@ import { extension } from "../scss/stuff.js";
 
 export const name = "Bootstrap";
 
+export const emoji = "👢";
+
+export const usageMarkdown = ["You can use Bootstrap classes like `card` or `pt-5` in the markup (components, routes, `app.html`).", "You can [customize your Bootstrap theme with variables](https://getbootstrap.com/docs/5.1/customize/sass/) like `$primary` or `$grid-breakpoints` in `src/variables.scss`."];
+
 /** @type {import("../..").Gatekeep} */
 export const gatekeep = async () => {
 	return { able: true };
@@ -14,6 +18,7 @@ export const options = {
 	sveltestrap: {
 		context: "It provides Svelte components with client-side interactivity in Bootstrap's style. https://sveltestrap.js.org/",
 		default: false,
+		descriptionMarkdown: "whether or not to install [Sveltestrap](https://github.com/bestguy/sveltestrap).",
 		question: "Do you want to install Sveltestrap?",
 	},
 };
